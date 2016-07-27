3d4d1661a41799ed5daa361f42c2d67df94e8979
philhuang100 <phil.huang100@gmail.com>
2016-07-27 10:42:23 +0800

    首發

--- app/src/main/java/tw/com/pcschool/instantreplysystem_10/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/instantreplysystem_10/MainActivity.java
@@ -1,7 +1,9 @@
 package tw.com.pcschool.instantreplysystem_10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void importClick(View v)
+    {
+        Intent it = new Intent(MainActivity.this, ImportActivity.class);
+        startActivity(it);
+    }
+    public void checkInClick(View v)
+    {
+        Intent it = new Intent(MainActivity.this, CheckInActivity.class);
+        startActivity(it);
+    }
+    public void replyClick(View v)
+    {
+        Intent it = new Intent(MainActivity.this, ReplyActivity.class);
+        startActivity(it);
+    }
+    public void scheduleClick(View v)
+    {
+        Intent it = new Intent(MainActivity.this, ScheduleActivity.class);
+        startActivity(it);
     }
 }
---
+++ app/src/main/java/tw/com/pcschool/instantreplysystem_10/ScheduleActivity.java
@@ -0,0 +1,13 @@
+package tw.com.pcschool.instantreplysystem_10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
