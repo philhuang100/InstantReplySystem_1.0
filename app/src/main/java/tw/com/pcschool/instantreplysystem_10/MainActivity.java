3b8496b8346729f6763d475282a745931cca70be
philhuang100 <phil.huang100@gmail.com>
2016-07-27 15:59:36 +0800

    首發

--- app/src/main/java/tw/com/pcschool/instantreplysystem_10/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/instantreplysystem_10/MainActivity.java
@@ -1,10 +1,12 @@
 package tw.com.pcschool.instantreplysystem_10;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,6 +16,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_my_dialog);
+        // 取得自定义View
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View myLoginView = layoutInflater.inflate(R.layout.activity_my_dialog, null);
+
+        Dialog alertDialog = new AlertDialog.Builder(this).
+                setTitle("使用者登入").
+                // setIcon(R.drawable.ic_launcher).
+                        setView(myLoginView).
+                        setPositiveButton("確定", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO Auto-generated method stub
+                            }
+                        }).
+                        setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO Auto-generated method stub
+                            }
+                        }).
+                        create();
+        alertDialog.show();
         setContentView(R.layout.activity_main);
     }
     public void importClick(View v)
