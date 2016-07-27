6c4fd52f4b804f9440ae37a9a6cf75e5cd06c888
philhuang100 <phil.huang100@gmail.com>
2016-07-27 15:18:30 +0800

    aa

--- app/src/main/java/tw/com/pcschool/instantreplysystem_10/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/instantreplysystem_10/MainActivity.java
@@ -1,9 +1,13 @@
 package tw.com.pcschool.instantreplysystem_10;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent it = new Intent(MainActivity.this, ScheduleActivity.class);
         startActivity(it);
+    }
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("登入訊息");
+        builder.setMessage("帳號");
+        builder.setMessage("密碼");
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+
+
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str = ed.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+        builder.create().show();
     }
 }
